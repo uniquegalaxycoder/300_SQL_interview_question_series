a5a75f124f6500ba0f72ce989fe7f1c162489c3d
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2025-12-25 14:58:45 +0530

    Create customer and sales tables with sample data
    This SQL script creates customer and sales tables, inserts sample data, and queries customers who have never made a purchase.

---
+++ 12 Find the customer details who have never had purchase.sql
@@ -0,0 +1,59 @@
+create table customer (
+  cust_id int not null,
+  cust_name varchar(30) not null,
+  app_signup_date date  not null,
+  cust_location varchar(80),
+  cust_phone_no bigint,
+  verfiy_status boolean
+);
+
+create table sales (
+  sales_id int not null,
+  product_name varchar(30) not null,
+  product_id int not null,
+  customer_id int not null,
+  sales_date date not null,
+  sales_amount float not null,
+  transaction_status boolean
+);
+
+insert into customer(cust_id, cust_name, app_signup_date, cust_location, cust_phone_no, verfiy_status) 
+values 
+(1, 'Joseph', '2025-01-01', 'New York City', 1234567890, 1),
+(2, 'Robert', '2024-08-11', 'Los Angeles', 2345678901, 1),
+(3, 'Kyle', '2024-11-23', 'Chicago', 1230987654, 0),
+(4, 'Antonio', '2023-04-13', 'Houston', 1234098765, 1),
+(5, 'Louis', '2024-09-21', 'Philadelphia', 9987654321, 1),
+(6, 'Stephan', '2024-12-26', 'New York City', 4567890321, 1 ),
+(7, 'Kyle', '2024-11-23', 'Chicago', 0987612345, 1) ;
+
+insert into sales (sales_id, product_name, product_id, customer_id, sales_date, sales_amount, transaction_status)
+values 
+(106, 'Chai Cup Bulk', 506, 1, '2025-11-29', 500.00, 1),
+(107, 'CCD Coffee Pack', 507, 2, '2025-11-30', 750.25, 1),
+(108, 'Tapri Startup Guide', 508, 5, '2025-12-01', 1200.00, 0),
+(109, 'Red Rooster Exhaust', 509, 5, '2025-12-02', 14000.00, 1),
+(110, 'Chaayos Corporate Kit', 510, 6, '2025-12-03', 18000.00, 1),
+(101, 'GT 650 Exhaust', 501, 1, '2025-11-24', 12500.75, 1),
+(102, 'KP Racing Exhaust', 502, 2, '2025-11-25', 13500.00, 1),
+(103, 'JBL Tune 720', 503, 9003, '2025-11-26', 4500.50, 0),
+(104, 'Corporate Tea Pack', 504, 4, '2025-11-27', 850.00, 1),
+(105, 'CSB Franchise Kit', 505, 4, '2025-11-28', 22000.00, 1);
+
+select * from customer ;
+select * from sales ;
+
+-- Q. Find the customers who have not made any purchase
+
+select 
+  a.cust_id,
+  a.cust_name,
+  a.app_signup_date,
+  a.cust_location,
+  a.cust_phone_no
+from customer as a 
+left join sales as b 
+on a.cust_id =  b.customer_id
+where 
+a.verfiy_status = True
+and ( b.customer_id is null or b.sales_id is null )
