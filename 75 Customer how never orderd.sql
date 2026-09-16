e92bf905e2bd2831f532f794a7db352535392fbd
Omkar Ghodake <uniquegalaxy2000@gmail.com>
2026-09-16 23:59:51 +0530

    Refactor SQL script for customer order analysis
    Updated SQL script to create Customers and Orders tables, insert sample data, and provide multiple solutions to find customers who never ordered.

--- 75 Customer how never orderd.sql
+++ 75 Customer how never orderd.sql
@@ -1,12 +1,60 @@
+-- find those customers who never ordered
+
+Create table Customers (
+  id int,
+  name varchar(255)
+  ) ;
+
+Create table Orders (
+  id int,
+  customerId int
+) ;
 
 
-Create table If Not Exists Customers (id int, name varchar(255))
-Create table If Not Exists Orders (id int, customerId int)
-Truncate table Customers
-insert into Customers (id, name) values ('1', 'Joe')
-insert into Customers (id, name) values ('2', 'Henry')
-insert into Customers (id, name) values ('3', 'Sam')
-insert into Customers (id, name) values ('4', 'Max')
-Truncate table Orders
-insert into Orders (id, customerId) values ('1', '3')
-insert into Orders (id, customerId) values ('2', '1')
+insert into Customers (id, name) values 
+  ('1', 'Joe'),
+  ('2', 'Henry')
+  ('3', 'Sam')
+  ('4', 'Max') ;
+
+insert into Orders (id, customerId) values
+  ('1', '3')
+  ('2', '1') ;
+
+
+-- Solution 1 
+with cte1 as (
+  select 
+      a.name as Customers,
+      b.customerId
+  from customers as a 
+    left join 
+      orders as b 
+  on a.id = b.customerid 
+  where b.id is null 
+)
+
+select Customers from cte1 ;
+
+
+-- solution 2 
+select name as customers from customers 
+where id not in (select customerid from orders) ;
+
+
+-- solution 3 
+select 
+  c.name as customers
+from customers as c 
+where not exists ( select 1 from orders as o where o.customerid = c.id ); 
+
+-- output 
+"""
+| Customers |
+| --------- |
+| Henry     |
+| Max       |
+  
+  """
+
+
